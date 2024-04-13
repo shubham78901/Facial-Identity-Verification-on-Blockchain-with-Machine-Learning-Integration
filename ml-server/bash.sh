03cb5d412a0d3b7d55638f5605df304e7e5475b3
shubham78901 <ss363757@gmail.com>
2024-04-13 13:57:36 +0530

    remove ml-server

--- ml-server/bash.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install PyTorch
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
-
-# Run the FastAPI application
-uvicorn main:app --host 0.0.0.0 --port 10000
